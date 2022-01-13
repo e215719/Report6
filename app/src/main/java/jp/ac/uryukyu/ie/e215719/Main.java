c39567e2b5bef29f14e3fbff598173c923855a04
Ryuki IKEHARA <e215719@ie.u-ryukyu.ac.jp>
2022-01-13 20:07:19 +0900

    ループ

--- app/src/main/java/jp/ac/uryukyu/ie/e215719/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215719/Main.java
@@ -6,10 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Board board = new Board();
         Scanner scanner = new Scanner(System.in);
-        String line = "";
-        System.out.println(board.getDisplayString()); //盤を出力
-        line = scanner.nextLine(); //一行読み取り
-        board.move(line); //駒を動かす
-        System.out.println(board.getDisplayString());
+        String line;
+        while (true) {
+            System.out.println(board.getDisplayString()); //盤を出力
+            line = scanner.nextLine(); //一行読み取り
+            board.move(line); //駒を動かす
+            System.out.println(line);
+        }
     }
 }
